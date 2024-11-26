135633a658c283d71cab4d10402e8e4625f99ee4
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-11-26 09:03:43 -0300

    Exemplo de utilização da biblioteca useEffect para manipulação do DOM em react.

--- usuario/src/App.jsx
+++ usuario/src/App.jsx
@@ -1,33 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
+import Counter from './Counter';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [task, setTask] = useState('');
-
-  const addTask = () => {
-    if(task) {
-      setTodos([...todos, task]);
-      setTask('');
-    }
-  }
-
   return(
-    <div>
-      <h1>Lista de tarefas</h1>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => {setTask(e.target.value)}}
-        placeholder="Insira a tarefa"
-      />
-      <button onClick={addTask}>Add Tarefa</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
+    <div className='App'>
+      <h1>Hook Effects</h1>
+      <Counter />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ usuario/src/Counter.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `Você clicou ${count} vezes`;
+    }, [count]);
+
+    return(
+        <div>
+            <p>Você clicou {count} vezes</p>
+            <button onClick={() => setCount(count + 1)}>
+                Clique aqui
+            </button>
+        </div>
+    );
+}
+
+export default Counter;
